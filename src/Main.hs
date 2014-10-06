d020a76f82e57b41e2f10837a1e0faffe80a2cae
Michael Blockley <michael@sss.co.nz>
2014-10-07 11:35:42 +1300

    added !syshealth

--- src/Main.hs
+++ src/Main.hs
@@ -10,11 +10,13 @@ main = runPlugin "sysup" "0.1.0.0" sysup runHarkerClient
 sysup :: HarkerClient ()
 sysup = do
     msg <- getMsg
-    if msg == "!help"      then help
-    else when (msg == "!sysup") sysupmsg
+    if msg == "!help"           then help
+    else if msg == "!syshealth" then syshealthmsg
+    else when (msg == "!sysup")      sysupmsg
 
 help :: HarkerClient ()
-help = sendReply "!sysup: shows the current system uptime"
+help = sendReply "!sysup:     shows the current system uptime"
+    >> sendReply "!syshealth: shows the current system load"
 
 sysupmsg :: HarkerClient ()
 sysupmsg = lift (readProcess "uptime" [] []) >>= sendReply . getTime
@@ -22,3 +24,8 @@ sysupmsg = lift (readProcess "uptime" [] []) >>= sendReply . getTime
     getTime = takeWhile (/= ',') . dropHead
     dropHead = dropAndTail (/= ' ') . dropAndTail (/= ' ') . tail
     dropAndTail x = tail . dropWhile x
+
+syshealthmsg :: HarkerClient ()
+syshealthmsg = lift (readProcess "uptime" [] []) >>= sendReply . getHealth
+  where
+    getHealth = tail . reverse . tail . takeWhile (/= ':') . reverse
